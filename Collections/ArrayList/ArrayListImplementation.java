b7b765451f5b51bb71af1937ee7366581d4cf926
Rohit Ambulge <rohitambulge15@gmail.com>
2026-02-13 23:39:44 +0530

    Comparable

--- Collections/ArrayList/ArrayListImplementation.java
+++ Collections/ArrayList/ArrayListImplementation.java
@@ -4,13 +4,25 @@ class UserIllegalArgumentException extends RuntimeException {
     }
 }
 
+class UserNullPointerException extends RuntimmeException{
+    public UserNullPointerException(String desc){
+        super(desc);
+    }
+}
+
+class UserNoSuchElementException extends RuntimeException{
+    public UserNoSuchElementException(){
+        super();
+    }
+}
+
 class UserArrayList<E>{
     private E [] arr;
     private int index;
     private static final int DEFAULT_CAPACITY = 10;
 
     public UserArrayList(){
-        this(DEFAULT_CAPACITY);
+        arr= (E[]) new Object[DEFAULT_CAPACITY];
     }
 
     public UserArrayList(int initialCapacity){
@@ -27,18 +39,81 @@ class UserArrayList<E>{
         this.index = 0;
     }
 
+    // public userArrayList(Collection<? extends E> c){
+    //     if(c == null){
+    //         throw new UserNullPointerException("Input Collection cannot be null");
+    //     }
+
+    //     if(c instanceOf UserArrayList){
+    //         UserArrayList<? extends E> otherList = (UserArrayList<? extends E>)c;
+    //         this.index = otherList.size();
+    //     }
+    // }
 
 
+    public boolean add(E ele){
+        if(index == arr.length){
+            int newCap = newCapacity(arr.length);
+            E[] newArr = (E[])new Object[newCap];
+            int i = 0;
+            for(E ele : arr){
+                newArr[i++] ele;
+            }
+            err = newArr;
+        }
+
+        arr[index++] = ele;
+        retrun true;
+    }
+
+    private int newCapacity(int oldCap){
+        return (int)(oldCap * 1.5);
+    }
 
     public int size() {
         return index;
     }
 
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public void clear(){
+        for(int i=0;i<index;i++){
+            arr[i] = null;
+        }
+        index = 0;
+    }
+
+    public E getFirst(){
+        if(size() == 0){
+            throw new UserNoSuchElementException();
+        }
+
+        return arr[0];
+    }
+
+    publix E getLast(){
+        if(size() == 0){
+            throw new UserNoSuchElementException();
+        }
+
+        return arr[index-1];
+    }
+
+    @Override
     public String toString(){
         if(size() == 0){
             return "[]";
         }
-        return "";
+        
+        String op = "[";
+        for(int i=0;i<index-1;i++){
+            op += arr[i]+", ";
+        }
+        op += arr[index-1]+"]";
+
+        return op;
     }
 }
 
---
+++ Comparable/StudentDriver.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student implements Comparable{
+    String name;
+    int age;
+    long phone;
+    double per;
+    String deg;
+
+    public Student(){
+
+    }
+
+    public Student(String name, int age, long phone, double per, String deg){
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.per = per;
+        this.deg = deg;
+    }
+
+    public String toString(){
+        return "[name : "+name+" , age : "+age+", phone : "+phone+", per : "+per+", deg : "+deg+"]";
+    }
+
+
+    public int compareTo(Student ref){
+        return (this.per > ref.per)?-1:(this.per < ref.per)?1:0;
+        // return this.name.compareTo(ref.name);
+    }
+
+}
+
+public class StudentDriver{
+    public static void main(String[] args){
+
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student("Vaibhav", 22,7058114426l, 80, "Bcom"));
+       list.add( new Student("Rohan", 25,7057114426l, 45, "B.tech"));
+        list.add(new Student("Abhay", 23,7058214426l, 69, "Bsc"));
+        list.add(new Student("Rutik", 24,7058164426l, 67, "Bca"));
+       list.add( new Student("Babar", 21,7058114486l, 53, "Ba"));
+
+    //    for(Student s : list){
+    //      System.out.println(s);
+    //    }
+        
+        Collections.sort(list);
+        // System.out.println(list);
+
+        for(Student st : list){
+            System.out.println(st);
+        }
+
+    }
+
+}
